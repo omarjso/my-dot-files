d3980379cca9833711860fa53f86e29ff11db71d
Omar Shaikh Omar <omarshaikhomar@gmail.com>
2025-01-06 09:32:30 +0300

    Add another mistyping

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,6 +3,7 @@
 -- Add any additional keymaps here
 
 vim.api.nvim_create_user_command("Q", "q", {}) -- :Q -> :q
+vim.api.nvim_create_user_command("W", "w", {}) -- :W -> :w
 vim.api.nvim_create_user_command("WQ", "wq", {}) -- :WQ -> :wq
 vim.api.nvim_create_user_command("Wq", "wq", {}) -- :Wq -> :wq
 
