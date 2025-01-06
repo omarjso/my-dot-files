a6f1bb068147b17307b29c1d0620d1b44ec9ca2b
Omar Shaikh Omar <omarshaikhomar@gmail.com>
2025-01-06 09:33:07 +0300

    Modify cursor to be block in all modes and highlight line number

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,4 +2,18 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.cursorline = false
+-- Indentation
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.smarttab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+
+-- Cursor
+vim.opt.cursorlineopt = "number"
+vim.api.nvim_set_hl(0, "CursorLineNr", {
+  fg = "#FFD700",
+  bold = true,
+})
+vim.opt.guicursor = "n-c-v:block-nCursor"
