482c87a6c9003ff318b4422f93625e45b4cedb47
Omar Shaikh Omar <omarshaikhomar@gmail.com>
2025-02-04 11:16:39 +0300

    Add auto style for JS files

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,13 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Set tab size to 2 for JavaScript, TypeScript, and related file types
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "javascript", "typescript", "jsx", "tsx", "json" },
+  callback = function()
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.expandtab = true
+  end,
+})
