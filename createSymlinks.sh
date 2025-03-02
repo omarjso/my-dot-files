a151a525518224fbdcb4b8fa66b74358698eab7d
Omar Shaikh Omar <omarshaikhomar@gmail.com>
2025-03-02 14:35:11 +0300

    Add sioyek config

--- createSymlinks.sh
+++ createSymlinks.sh
@@ -3,3 +3,6 @@ ln -sf ~/dot_files/nvim ~/.config/
 ln -sf ~/dot_files/.zshrc ~/.zshrc
 ln -sf ~/dot_files/.tmux.conf ~/.tmux.conf
 ln -sf ~/dot_files/.gitconfig ~/.gitconfig
+for file in ~/dot_files/sioyek.app/*.config; do
+    ln -sf "$file" /Applications/sioyek.app/Contents/MacOS/
+done
