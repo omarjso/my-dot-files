0a706037c3bf51e43396c71803239429cb5ac75d
Omar Shaikh Omar <omarshaikhomar@gmail.com>
2025-09-06 10:51:27 +0300

    Update symlink script with config file checking

--- createSymlinks.sh
+++ createSymlinks.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-ln -sf ~/dot_files/nvim ~/.config/
-ln -sf ~/dot_files/.zshrc ~/.zshrc
-ln -sf ~/dot_files/.tmux.conf ~/.tmux.conf
-ln -sf ~/dot_files/.gitconfig ~/.gitconfig
-for file in ~/dot_files/sioyek.app/*.config; do
-    ln -sf "$file" ~/Library/Application\ Support/sioyek/
-done
+set -e
+
+XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+mkdir -p "$XDG_CONFIG_HOME"
+
+ln -sf "$HOME/dot_files/nvim" "$XDG_CONFIG_HOME/nvim"
+ln -sf "$HOME/dot_files/.zshrc" "$HOME/.zshrc"
+ln -sf "$HOME/dot_files/.tmux.conf" "$HOME/.tmux.conf"
+ln -sf "$HOME/dot_files/.gitconfig" "$HOME/.gitconfig"
+
+if [ -n "$SIOYEK_CONFIG_DIR" ]; then
+    mkdir -p "$SIOYEK_CONFIG_DIR"
+    for file in "$HOME/dot_files/sioyek.app"/*.config; do
+        ln -sf "$file" "$SIOYEK_CONFIG_DIR/"
+    done
+fi
